35e0c2743f49250dd0f26ea8618f9a627f5ef8cd
peterjjonas <peterjjonas@timeravel.studio>
2021-05-10 15:02:04 +0100

    Add first line by JavaScript

--- maze.js
+++ maze.js
@@ -1,2 +1,23 @@
 let mazeWidth = 8;
 let mazeHeight = 8;
+
+let maze = document.getElementById('maze');
+let corridor = document.getElementById('corridor');
+let ul = document.querySelector("ul");
+// let li = document.getElementsByTagName("li");
+
+
+function drawMaze() {
+	// div.appendChild(document.createTextNode('AAA'));
+  for (w = 0; w < mazeWidth; w++) {
+    var div = document.createElement('div');
+    ul.appendChild(div).classList.add('corridor');
+    console.log(w);
+  }
+}
+
+function reDrawMaze() {
+    corridor.style.background = 'red';
+}
+
+drawMaze();
