58363b83a8df67dac47c98af7e26bae89402bdd3
peterjjonas <peterjjonas@timeravel.studio>
2021-05-11 14:14:31 +0100

    Add createPlainMaze and generateMaze basic functions

--- maze.js
+++ maze.js
@@ -1,23 +1,37 @@
 let mazeWidth = 8;
-let mazeHeight = 8;
+let mazeHeight = 10;
 
 let maze = document.getElementById('maze');
 let corridor = document.getElementById('corridor');
 let ul = document.querySelector("ul");
-// let li = document.getElementsByTagName("li");
 
+function createPlainMaze(mazeHeight, mazeWidth) {
+  let plainMaze = new Array(mazeHeight);
+  for (let width = 0; width < mazeHeight; width++) {
+    plainMaze[width] = new Array(mazeWidth);
+  }
+  return plainMaze;
+}
 
-function drawMaze() {
-	// div.appendChild(document.createTextNode('AAA'));
+function generateMaze(mazeHeight, mazeWidth) {
+  let randomMaze = new createPlainMaze(mazeHeight, mazeWidth);
+  for (let height = 0; height < mazeHeight; height++) {
+    for (let width = 0; width < mazeWidth; width++) {
+      randomMaze[height][width] = 0;
+    }
+  }
+  return randomMaze;
+}
+
+function drawMaze(mazeHeight, mazeWidth) {
   for (w = 0; w < mazeWidth; w++) {
     var div = document.createElement('div');
-    ul.appendChild(div).classList.add('corridor');
-    console.log(w);
+    ul.appendChild(div).classList.add('wall');
   }
 }
 
-function reDrawMaze() {
-    corridor.style.background = 'red';
-}
+let randomMaze = new generateMaze(mazeHeight, mazeWidth);
 
-drawMaze();
+console.table(randomMaze);
+
+drawMaze(2, 2);
