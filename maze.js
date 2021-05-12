e3a22b14d71d78cb96a23324b9fa639047272168
peterjjonas <peterjjonas@timeravel.studio>
2021-05-12 13:38:58 +0100

    Clean up the code

--- maze.js
+++ maze.js
@@ -1,40 +1,8 @@
 let mazeHeight = 11;
 let mazeWidth = 13;
-
 const maze = document.getElementById('maze');
 
-const createMazeBorder = (mazeHeight, mazeWidth) => {
-  const mazeHeightPixel = mazeHeight * 20;
-  maze.style.height = mazeHeightPixel + 'px';
-  const mazeWidthPixel = mazeWidth * 20;
-  maze.style.width = mazeWidthPixel + 'px';
-}
-
-function createPlainMaze(mazeHeight, mazeWidth) {
-  const plainMaze = new Array(mazeHeight);
-  for (height = 0; height < mazeHeight; height++) {
-    plainMaze[height] = new Array(mazeWidth);
-  }
-  return plainMaze;
-}
-
-function generateMaze(mazeHeight, mazeWidth) {
-  const randomMaze = new createPlainMaze(mazeHeight, mazeWidth);
-  for (width = 0; width < mazeWidth; width++) {
-    for (height = 0; height < mazeHeight; height++) {
-      const makeWall = getRandomWall();
-      randomMaze[height][width] = makeWall;
-    }
-  }
-  return randomMaze;
-}
-
-function getRandomWall() {
-  const randomWall = Math.floor(Math.random() * 2);
-  return randomWall;
-}
-
-function drawMaze(mazeHeight, mazeWidth) {
+const drawMaze = (mazeHeight, mazeWidth) => {
   for (height = 0; height < mazeHeight; height++) {
     for (width = 0; width < mazeWidth; width++) {
       const div = document.createElement('div');
@@ -47,8 +15,38 @@ function drawMaze(mazeHeight, mazeWidth) {
   }
 }
 
-const randomMaze = new generateMaze(mazeHeight, mazeWidth);
-console.table(randomMaze);
+const createMazeBorder = (mazeHeight, mazeWidth) => {
+  const mazeHeightPixel = mazeHeight * 20;
+  maze.style.height = mazeHeightPixel + 'px';
+  const mazeWidthPixel = mazeWidth * 20;
+  maze.style.width = mazeWidthPixel + 'px';
+}
+
+const createRandomWall = () => {
+  const randomWall = Math.floor(Math.random() * 2);
+  return randomWall;
+}
+
+function createPlainMaze(mazeHeight, mazeWidth) {
+  const plainMaze = new Array(mazeHeight);
+  for (height = 0; height < mazeHeight; height++) {
+    plainMaze[height] = new Array(mazeWidth);
+  }
+  return plainMaze;
+}
+
+function generateRandomMaze(mazeHeight, mazeWidth) {
+  const randomMaze = new createPlainMaze(mazeHeight, mazeWidth);
+  for (width = 0; width < mazeWidth; width++) {
+    for (height = 0; height < mazeHeight; height++) {
+      const createWall = createRandomWall();
+      randomMaze[height][width] = createWall;
+    }
+  }
+  return randomMaze;
+}
+
+const randomMaze = new generateRandomMaze(mazeHeight, mazeWidth);
 
 createMazeBorder(mazeHeight, mazeWidth);
 
